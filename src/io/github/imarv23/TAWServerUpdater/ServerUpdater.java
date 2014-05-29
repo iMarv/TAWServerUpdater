b62d5d6d9e464b89baaf6b4ac5bd6cdfdbe79ccb
iMarv <git.imarv@web.de>
2014-05-29 18:38:45 +0200

    Added arguments
    Added arguments to specify what should be updated and how

--- src/io/github/imarv23/TAWServerUpdater/ServerUpdater.java
+++ src/io/github/imarv23/TAWServerUpdater/ServerUpdater.java
@@ -1,13 +1,26 @@
 package io.github.imarv23.TAWServerUpdater;
 
+import io.github.imarv23.TAWServerUpdater.extra.AllPluginsUpdater;
 import io.github.imarv23.TAWServerUpdater.files.FileManager;
+import io.github.imarv23.TAWServerUpdater.files.PluginManager;
 
 public class ServerUpdater {
 
 	public static void main(String[] args) {
 		try{
-			FileManager fm = new FileManager(args[0]);
-			fm.checkForUpdates(args[1]);
+			if("server".equals(args[2]))
+			{
+				FileManager fm = new FileManager(args[0]);
+				fm.checkForUpdates(args[1]);
+			}else if("plugin".equals(args[2]))
+			{
+				PluginManager pm = new PluginManager(args[0]);
+				pm.checkForUpdates(args[1]);
+			}else if("allplugins".equals(args[2]))
+			{
+				AllPluginsUpdater apu = new AllPluginsUpdater(args[0], args[1]);
+				apu.update();
+			}
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
